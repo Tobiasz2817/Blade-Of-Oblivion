09c7fc88ef98495b41ebc0faad6d58fbfa78d024
Tobiasz2817 <Tobiasz2817@outlook.com>
2023-06-07 12:16:11 +0200

    Spider anims part 1

---
+++ Assets/Game/Scripts/AI/Base/AnimateMovementByNavMesh.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateMovementByNavMesh : MonoBehaviour
+{
+    [SerializeField] private string moveX;
+    [SerializeField] private string moveZ;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
